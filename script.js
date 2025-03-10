5c88c1aa28a592f894a8395a072bc01d058489da
Sova42-cmd <steamsova42@gmail.com>
2025-03-10 15:34:03 +0400

    Added base-start, and did some js basics

--- script.js
+++ script.js
@@ -9,3 +9,15 @@
 // console.log(myAge)
 
 // lesson 2. array
+
+let num = 17
+const isStudent = "David"
+
+// symbols $ and _ are allowed for some reason
+
+// Restricted -> numbers in beginning, - symbol, and service names
+// let 42num = 11
+// let my-num = 1
+// let const/let
+
+alert(num)
