852a9e32778ec4ca884b9f5cb6b8c6e891653aa5
Sova42-cmd <steamsova42@gmail.com>
2025-03-13 18:23:32 +0400

    Practicing theory - Operators, Data types, Priority

--- script.js
+++ script.js
@@ -1,108 +1,3 @@
-// // let myAge = 17
-// // let myCurrentAge = myAge + 1
-// // console.log(myCurrentAge)
-//
-// // let myName = "David"
-// // let myAge = 17
-//
-// // let myFriendName = "Aram"
-// // let myFriendAge = 47
-//
-// // function greeting(sourceName, sourceAge) {
-// //     return "Hello my name " + sourceName + " I'm " + sourceAge + " Years old."
-// // }
-//
-// // const greeting = (sourceName, sourceAge) => {
-// //
-// //     const myComputer = "Mac"
-// //
-// //     return "Hello my name " + sourceName + " I'm " + sourceAge + " Years old."
-// // }
-// //
-// //
-// // console.log(greeting(myName, myAge))
-// // console.log(greeting(myFriendName, myFriendAge))
-//
-//
-// // function ageChecker(sourceAge) {
-// //
-// //     const result=sourceAge>30?"I'm too old":"I'm too young"
-// //
-// //     return result
-// // }
-//
-// // const ageChecker = (sourceAge) => {
-// //     const result = sourceAge > 30 ? "I'm too old" : "I'm too young"
-// //     return result
-// //
-// // }
-// //
-// // console.log(ageChecker(40))
-// // console.log(ageChecker(11))
-// // console.log(ageChecker(12))
-// // console.log(ageChecker(89))
-//
-// // function addingNumbers(num1, num2) {
-// //
-// //     const sum=num1+num2
-// //
-// //     const result = sum>100?"Nah, you calculate this, but here's the result anyway -> "+sum:sum
-// //
-// //     return result
-// // }
-// //
-// // console.log(addingNumbers(180, 28))
-//
-// // 3 parameter, if 3rd on is string, then sum and result, if not sum I don't undersdant you'
-//
-// // function counterBeta(sourceNum1, sourceNum2, sourceName) {
-// //
-// //     const sum = sourceNum1 + sourceNum2;
-// //
-// //     const result=sourceName==="plus"?sum:"I don't understand you"
-// //
-// //     return result
-// // }
-// //
-//
-// function addNumberCheck(num1, num2, sourceName) {
-//
-//     const sum = num1 + num2;
-//
-//     const sub = num1 - num2;
-//
-//     const multiply = num1 * num2;
-//
-//     const divide = num1 / num2;
-//
-//     let result
-//
-//     if (sourceName === "Add") {
-//         result = sum
-//     }
-//
-//     if (sourceName === "Minus") {
-//         result = sub
-//     }
-//
-//     if (sourceName === "Times") {
-//         result = multiply
-//     }
-//
-//     if (sourceName === "Divide") {
-//         result = divide
-//     } e
-//
-//     return result
-// }
-//
-// console.log(addNumberCheck(58, 91, "Add"))
-// console.log(addNumberCheck(58, 91, "Minus"))
-// console.log(addNumberCheck(58, 91, "Times"))
-// console.log(addNumberCheck(58, 91, "Divide"))
-// console.log(addNumberCheck(58, 91, "random"))
-//
-//
 //
 // let myAge = 17
 // // let myCurrentAge = myAge + 1
@@ -208,51 +103,6 @@
 // console.log(addNumberCheck(58, 91, "Divide"))
 // console.log(addNumberCheck(58, 91, "random"))
 //
-//
-//
-// let myAge = 17
-// // let myCurrentAge = myAge + 1
-// // console.log(myCurrentAge)
-//
-// // let myName = "David"
-// // let myAge = 17
-//
-// // let myFriendName = "Aram"
-// // let myFriendAge = 47
-//
-// // function greeting(sourceName, sourceAge) {
-// //     return "Hello my name " + sourceName + " I'm " + sourceAge + " Years old."
-// // }
-//
-// // const greeting = (sourceName, sourceAge) => {
-// //
-// //     const myComputer = "Mac"
-// //
-// //     return "Hello my name " + sourceName + " I'm " + sourceAge + " Years old."
-// // }
-// //
-// //
-// // console.log(greeting(myName, myAge))
-// // console.log(greeting(myFriendName, myFriendAge))
-//
-//
-// // function ageChecker(sourceAge) {
-// //
-// //     const result=sourceAge>30?"I'm too old":"I'm too young"
-// //
-// //     return result
-// // }
-//
-// // const ageChecker = (sourceAge) => {
-// //     const result = sourceAge > 30 ? "I'm too old" : "I'm too young"
-// //     return result
-// //
-// // }
-// //
-// // console.log(ageChecker(40))
-// // console.log(ageChecker(11))
-// // console.log(ageChecker(12))
-// // console.log(ageChecker(89))
 //
 // // function addingNumbers(num1, num2) {
 // //
@@ -334,7 +184,24 @@
 const resultElement = document.getElementById("result");
 const input1=document.getElementById("input1");
 const input2=document.getElementById("input2");
+const submitBtn=document.getElementById("submit");
+const minusBtn=document.getElementById("minus");
+const plusBtn=document.getElementById("plus");
 
-const sum = Number(input1.value) + Number(input2.value);
-resultElement.textContent = sum
-console.log(typeof sum);
+
+// console.log(typeof sum);
+
+// submitBtn.onclick = function () {
+//     const sum = Number(input1.value) + Number(input2.value)
+//     resultElement.textContent = sum
+// }
+minusBtn.onclick = function () {
+    const sub = Number(input1.value) - Number(input2.value)
+    resultElement.textContent = sub
+}
+plusBtn.onclick = function () {
+    const sum = Number(input1.value) + Number(input2.value)
+    resultElement.textContent = sum
+}
+
+
---
+++ theory.js
@@ -0,0 +1,39 @@
+////////Operators
+
+let a = 1
+let b = 2
+let c = 3
+
+c = c + a
+console.log(c)
+// c = c - a
+// c = c * a
+// c = c / a
+
+// c += a
+// c -= a
+// c *= a
+// c /= a
+
+// c = 4 ** 2
+
+// Data types
+
+// const age = 19 // number
+// const name = "David"  //string
+// const isStudent = true //boolean
+// let x // undefined
+// console.log(x)
+// //null
+
+// Priority
+
+const fullAge = 17
+const birthYear = 2007
+const currentYear = 2025
+// > <
+const isFullAge = currentYear - birthYear < fullAge
+console.log(isFullAge)
+
+
+
