3f3905461aad937dc311dfd487e96ac82dad540b
Sova42-cmd <steamsova42@gmail.com>
2025-03-15 11:56:25 +0400

    Refactored -> listElement.insertAdjacentHTML

--- app.js
+++ app.js
@@ -10,19 +10,35 @@ const listElement = document.getElementById("list")
 
 // console.log(inputElement.value)
 
+const notes = ["Necessary notes 1", "Super necessary notes 2"]
+
+function render() {
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[0]))
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[1]))
+}
+
+render()
+
 createBtn.onclick = function () {
+    if (inputElement.value.length === 0) {
+        return
+    }
     // listElement.innerHTML =
     listElement.insertAdjacentHTML('beforeend',
 
-        `
+        getNoteTemplate(inputElement.value)
+    )
+    inputElement.value = ''
+}
+function getNoteTemplate(title) {
+    return `
       
          <li class="list-group-item d-flex justify-content-between align-items-center">
-                <span>${inputElement.value}</span>
+                <span>${title}</span>
                 <span>
                 <span class="btn btn-small btn-success">&check;</span>
                 <span class="btn btn-small btn-danger">&times;</span>
               </span>
             </li>
 `
-    )
 }
